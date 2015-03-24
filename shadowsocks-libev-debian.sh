44d7df50e39cf758621ba915db4c61a3ca307ddc
Teddysun <i@teddysun.com>
2015-03-24 17:04:12 +0800

    Update get public IP address method

--- shadowsocks-libev-debian.sh
+++ shadowsocks-libev-debian.sh
@@ -73,8 +73,8 @@ function pre_install(){
     apt-get install -y wget unzip curl build-essential autoconf libtool libssl-dev
     # Get IP address
     echo "Getting Public IP address, Please wait a moment..."
-    IP=$(curl -s checkip.dyndns.com | cut -d' ' -f 6  | cut -d'<' -f 1)
-    if [ $? -ne 0 -o -z $IP ]; then
+    IP=$(wget -qO- ipv4.icanhazip.com)
+    if [[ "$IP" = "" ]]; then
         IP=$(curl -s -4 ipinfo.io | grep "ip" | awk -F\" '{print $4}')
     fi
     echo -e "Your main public IP is\t\033[32m$IP\033[0m"
