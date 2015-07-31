32fd80131649fb4fdaf804f84e98e4dc81013b5f
Teddysun <i@teddysun.com>
2015-07-31 15:21:17 +0800

    Add input port option [1-65535]

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -72,12 +72,12 @@ function pre_install(){
     #Set shadowsocks-libev config port
     while true
     do
-    echo -e "Please input port for shadowsocks-libev [1024-65535]:"
+    echo -e "Please input port for shadowsocks-libev [1-65535]:"
     read -p "(Default port: 8989):" shadowsocksport
     [ -z "$shadowsocksport" ] && shadowsocksport="8989"
     expr $shadowsocksport + 0 &>/dev/null
     if [ $? -eq 0 ]; then
-        if [ $shadowsocksport -ge 1024 ] && [ $shadowsocksport -le 65535 ]; then
+        if [ $shadowsocksport -ge 1 ] && [ $shadowsocksport -le 65535 ]; then
             echo ""
             echo "---------------------------"
             echo "port = $shadowsocksport"
--- shadowsocks.sh
+++ shadowsocks.sh
@@ -87,12 +87,12 @@ function pre_install(){
     # Set shadowsocks config port
     while true
     do
-    echo -e "Please input port for shadowsocks-python [1024-65535]:"
+    echo -e "Please input port for shadowsocks-python [1-65535]:"
     read -p "(Default port: 8989):" shadowsocksport
     [ -z "$shadowsocksport" ] && shadowsocksport="8989"
     expr $shadowsocksport + 0 &>/dev/null
     if [ $? -eq 0 ]; then
-        if [ $shadowsocksport -ge 1024 ] && [ $shadowsocksport -le 65535 ]; then
+        if [ $shadowsocksport -ge 1 ] && [ $shadowsocksport -le 65535 ]; then
             echo ""
             echo "---------------------------"
             echo "port = $shadowsocksport"
