30619769c009a32e6909a5312fb9302a9c7963c9
Teddysun <i@teddysun.com>
2016-08-27 13:26:09 +0900

    update check shadowsocks status

--- shadowsocks-crond.sh
+++ shadowsocks-crond.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
-# Check Shadowsocks Server is running or not
-# Author: Teddysun <i@teddysun.com>
-# Visit: https://shadowsocks.be/6.html
+#=================================================================#
+#   System Required:  CentOS, Debian, Ubuntu                      #
+#   Description: Check Shadowsocks Server is running or not       #
+#   Author: Teddysun <i@teddysun.com>                             #
+#   Visit: https://shadowsocks.be/6.html                          #
+#=================================================================#
 
 # name
 name=Shadowsocks
 # log path
 path=/var/log
 # check log path
-[[ ! -d $path ]] && mkdir -p $path
+[[ ! -d ${path} ]] && mkdir -p ${path}
 # log file
-log=$path/shadowsocks-crond.log
+log=${path}/shadowsocks-crond.log
 # shadowsocks-python path(centos)
-bin1=/usr/bin/ssserver
+shadowsocks[0]=/usr/bin/ssserver
 # shadowsocks-python path(debian)
-bin2=/usr/local/bin/ssserver
+shadowsocks[1]=/usr/local/bin/ssserver
 # shadowsocks-go path
-bin3=/usr/bin/shadowsocks-server
+shadowsocks[2]=/usr/bin/shadowsocks-server
 # shadowsocks-libev path
-bin4=/usr/local/bin/ss-server
+shadowsocks[3]=/usr/local/bin/ss-server
 # shadowsocksR path
-bin5=/usr/local/shadowsocks/server.py
+shadowsocks[4]=/usr/local/shadowsocks/server.py
 # default pid value
 pid=""
+
+[ ! -f /etc/init.d/shadowsocks ] && echo "`date +"%Y-%m-%d %H:%M:%S"` /etc/init.d/shadowsocks is not existed" >> ${log} && exit
 
 # check Shadowsocks status
 /etc/init.d/shadowsocks status &>/dev/null
 if [ $? -eq 0 ]; then
-    pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${bin1}" | awk '{print $2}'` 
-    if [ -z $pid ]; then
-        pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${bin2}" | awk '{print $2}'` 
-        if [ -z $pid ]; then
-            pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${bin3}" | awk '{print $2}'` 
-            if [ -z $pid ]; then
-                pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${bin4}" | awk '{print $2}'` 
-                if [ -z $pid ]; then
-                    pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${bin5}" | awk '{print $2}'` 
-                fi
-            fi
+    for bin in ${shadowsocks[*]}
+    do
+        pid=`ps -ef | grep -v grep | grep -i "${bin}" | awk '{print $2}'` 
+        if [ ! -z ${pid} ]; then
+            break
         fi
-    fi
+    done
 fi
 
 # check status & auto start
-if [[ -z $pid ]]; then
-    echo "`date +"%Y-%m-%d %H:%M:%S"` $name is not running" >> $log
-    echo "`date +"%Y-%m-%d %H:%M:%S"` Starting $name" >> $log
+if [ -z ${pid} ]; then
+    echo "`date +"%Y-%m-%d %H:%M:%S"` $name is not running" >> ${log}
+    echo "`date +"%Y-%m-%d %H:%M:%S"` Starting $name" >> ${log}
     /etc/init.d/shadowsocks start &>/dev/null
     if [ $? -eq 0 ]; then
-        echo "`date +"%Y-%m-%d %H:%M:%S"` $name start success" >> $log
+        echo "`date +"%Y-%m-%d %H:%M:%S"` $name start success" >> ${log}
     else
-        echo "`date +"%Y-%m-%d %H:%M:%S"` $name start failed" >> $log
+        echo "`date +"%Y-%m-%d %H:%M:%S"` $name start failed" >> ${log}
     fi
 else
-    echo "`date +"%Y-%m-%d %H:%M:%S"` $name is running with pid $pid" >> $log
+    echo "`date +"%Y-%m-%d %H:%M:%S"` $name is running with pid $pid" >> ${log}
 fi
