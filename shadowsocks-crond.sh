13a10b6ba88ab37d0ccff731f9ab5edfcec76b93
Teddysun <i@teddysun.com>
2018-06-03 21:57:58 +0900

    Update comments
    Signed-off-by: Teddysun <i@teddysun.com>

--- shadowsocks-crond.sh
+++ shadowsocks-crond.sh
@@ -18,25 +18,25 @@ shadowsocks_init[3]=/etc/init.d/shadowsocks-go
 shadowsocks_init[4]=/etc/init.d/shadowsocks-libev
 
 i=0
-for init in ${shadowsocks_init[@]}; do
+for init in "${shadowsocks_init[@]}"; do
     pid=""
     if [ -f ${init} ]; then
-        ss_status=`${init} status`
+        ss_status=$(${init} status)
         if [ $? -eq 0 ]; then
-            pid=`echo $ss_status | sed 's/[^0-9]*//g'`
+            pid=$(echo "$ss_status" | sed -e 's/[^0-9]*//g')
         fi
 
-        if [ -z ${pid} ]; then
-            echo "`date +"%Y-%m-%d %H:%M:%S"` ${name[$i]} is not running" >> ${log}
-            echo "`date +"%Y-%m-%d %H:%M:%S"` Starting ${name[$i]}" >> ${log}
+        if [ -z "${pid}" ]; then
+            echo "$(date +'%Y-%m-%d %H:%M:%S') ${name[$i]} is not running" >> ${log}
+            echo "$(date +'%Y-%m-%d %H:%M:%S') Starting ${name[$i]}" >> ${log}
             ${init} start &>/dev/null
             if [ $? -eq 0 ]; then
-                echo "`date +"%Y-%m-%d %H:%M:%S"` ${name[$i]} start success" >> ${log}
+                echo "$(date +'%Y-%m-%d %H:%M:%S') ${name[$i]} start success" >> ${log}
             else
-                echo "`date +"%Y-%m-%d %H:%M:%S"` ${name[$i]} start failed" >> ${log}
+                echo "$(date +'%Y-%m-%d %H:%M:%S') ${name[$i]} start failed" >> ${log}
             fi
         else
-            echo "`date +"%Y-%m-%d %H:%M:%S"` ${name[$i]} is running with pid $pid" >> ${log}
+            echo "$(date +'%Y-%m-%d %H:%M:%S') ${name[$i]} is running with pid $pid" >> ${log}
         fi
     
     fi
