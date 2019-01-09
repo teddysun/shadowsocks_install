1259fb44037934502d900ca6e12fe0fd792c9d0c
huiyifyj <jxfengyijie@gmail.com>
2019-01-10 02:45:58 +0800

    Update libsodium_file and libsodium_url
    Update libsodium download link,
    [libsodium](https://download.libsodium.org/libsodium/releases/LATEST.tar.gz).
    To get the latest release.

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -37,8 +37,8 @@ plain='\033[0m'
 cur_dir=$( pwd )
 software=(Shadowsocks-Python ShadowsocksR Shadowsocks-Go Shadowsocks-libev)
 
-libsodium_file="libsodium-1.0.16"
-libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz"
+libsodium_file="libsodium-stable"
+libsodium_url="https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
 
 mbedtls_file="mbedtls-2.13.0"
 mbedtls_url="https://tls.mbed.org/download/mbedtls-2.13.0-gpl.tgz"
