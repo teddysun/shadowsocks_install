2f645b63cbc9d4495b4947c4b972ab6f34639d0d
Bintel <i@bintel.me>
2020-01-21 17:30:33 +0800

    bump version for mbed TLS

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -15,8 +15,8 @@ cur_dir=`pwd`
 libsodium_file="libsodium-stable"
 libsodium_url="https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
 
-mbedtls_file="mbedtls-2.16.3"
-mbedtls_url="https://tls.mbed.org/download/mbedtls-2.16.3-gpl.tgz"
+mbedtls_file="mbedtls-2.16.4"
+mbedtls_url="https://tls.mbed.org/download/mbedtls-2.16.4-gpl.tgz"
 
 # Stream Ciphers
 ciphers=(
