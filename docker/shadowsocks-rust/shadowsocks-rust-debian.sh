e8d1b9a82941221eca428abee1db765dce74bd6d
Teddysun <i@teddysun.com>
2020-11-05 15:59:46 +0900

    Added Dockerfile.debian

---
+++ docker/shadowsocks-rust/shadowsocks-rust-debian.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#
+# This is a Shell script for shadowsocks-rust based debian with Docker image
+# 
+# Copyright (C) 2019 - 2020 Teddysun <i@teddysun.com>
+#
+# Reference URL:
+# https://github.com/shadowsocks/shadowsocks-rust
+# https://github.com/shadowsocks/simple-obfs
+# https://github.com/shadowsocks/v2ray-plugin
+
+PLATFORM=$1
+if [ -z "$PLATFORM" ]; then
+    ARCH="x86_64-unknown-linux-gnu"
+else
+    case "$PLATFORM" in
+        linux/386)
+            ARCH=""
+            ;;
+        linux/amd64)
+            ARCH="x86_64-unknown-linux-gnu"
+            ;;
+        linux/arm/v6)
+            ARCH="arm-unknown-linux-gnueabihf"
+            ;;
+        linux/arm/v7)
+            ARCH=""
+            ;;
+        linux/arm64|linux/arm64/v8)
+            ARCH="aarch64-unknown-linux-gnu"
+            ;;
+        linux/ppc64le)
+            ARCH=""
+            ;;
+        linux/s390x)
+            ARCH=""
+            ;;
+        *)
+            ARCH=""
+            ;;
+    esac
+fi
+[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
+VERSION=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | grep 'tag_name' | cut -d\" -f4)
+[ -z "${VERSION}" ] && echo "Error: Get shadowsocks-rust latest version failed" && exit 1
+# Download shadowsocks-rust binary file
+SHADOWSOCKS_RUST_FILE="shadowsocks-${VERSION}.${ARCH}.tar.xz"
+SHADOWSOCKS_RUST_URL="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${VERSION}/${SHADOWSOCKS_RUST_FILE}"
+echo "Downloading shadowsocks-rust binary file: ${SHADOWSOCKS_RUST_FILE}"
+wget -O ${SHADOWSOCKS_RUST_FILE} ${SHADOWSOCKS_RUST_URL} > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download shadowsocks-rust binary file: ${SHADOWSOCKS_RUST_FILE}" && exit 1
+fi
+echo "Download shadowsocks-rust binary file: ${SHADOWSOCKS_RUST_FILE} completed"
+echo "Extracting ${SHADOWSOCKS_RUST_FILE}..."
+tar Jxf ${SHADOWSOCKS_RUST_FILE} -C /usr/bin
+chmod +x /usr/bin/ss*
+rm -f ${SHADOWSOCKS_RUST_FILE}
+echo "Install shadowsocks-rust binary file: ${SHADOWSOCKS_RUST_FILE} completed"
